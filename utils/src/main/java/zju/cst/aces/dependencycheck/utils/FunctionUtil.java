db74e3e5024bc04513cd33f1feaa485e19f0c48d
zhangyu49368 <767868683@qq.com>
2023-06-13 14:11:55 +0800

    vta displaypath

--- utils/src/main/java/zju/cst/aces/dependencycheck/utils/FunctionUtil.java
+++ utils/src/main/java/zju/cst/aces/dependencycheck/utils/FunctionUtil.java
@@ -136,7 +136,6 @@ public class FunctionUtil {
             for (ClassNode clazz : targetClasses) {
 
 
-
                 for (MethodNode method : clazz.methods) {
                     // skip non-public methods and abstract method
                     if (method.access != Opcodes.ACC_PUBLIC)
@@ -158,53 +157,44 @@ public class FunctionUtil {
 
     }
 
-    public static void findSourceMethod(SootMethod method, CallGraph newCg, String npi_class_method, String npijar) {
+    static ArrayList<String> pathJars = new ArrayList<>();
+    static ArrayList<String> pathMethods = new ArrayList<>();
+
+    public static boolean findSourceMethod(SootMethod method, CallGraph newCg, String npi_class_method, String npijar, boolean flag) {
 
         for (Iterator<Edge> it = newCg.edgesInto(method); it.hasNext(); ) {
+            //上一个src method 找到了
+            if (flag) return flag;
             Edge edge = it.next();
-//            LOGGER.info("All Methods {} Intruduce {}:",edge.src().getBytecodeSignature(),npi_class_method);
+            for (String ownjar : OWNJarsFunctions.keySet()) {
 
-//            for (String ownjar : OWNJarsFunctions.keySet()) {
-////                if(!Intro_relations.contains(new Pair<>(ownjar,npijar))){
-////                    continue;
-////                }
-//                String own_class_methodstr = OWNJarsFunctions.get(ownjar);
-//                String[] own_class_methods = own_class_methodstr.split(";");
-//                for (String own_class_method : own_class_methods) {
-//
-//                    if (own_class_method.equals(edge.src().getDeclaringClass().getName() + "_" + edge.src().getName())) {
-////                        if(!Intro_relations.contains(new Pair<>(ownjar,npijar))) {
-////                            break;
-////                        }
-////                        if(!Intro_relations.contains(new Pair<>(ownjar,npijar)))
-////                            Intro_relations.add(new Pair<>("一方库Jar包: "+ownjar,npijar));
-//                        System.out.println("一方库Jar包: " + ownjar + " -> 孤立Jar包: " + npijar);
-//                        System.out.println("一方库Method: " + own_class_method + " -> Method: " + npi_class_method);
-//                        return;
-////                        return;
-//                    }
-//                }
-//
-//            }
-//            for (String directjar : DIRECTJarsFunctions.keySet()) {
-////                if(!Intro_relations.contains(new Pair<>(directjar,npijar))){
-////                    continue;
-////                }
-//                String direct_class_methodstr = DIRECTJarsFunctions.get(directjar);
-//                String[] direct_class_methods = direct_class_methodstr.split(";");
-//                for (String direct_class_method : direct_class_methods) {
-//                    if (direct_class_method.equals(edge.src().getDeclaringClass().getName() + "_" + edge.src().getName())) {
-////                        if(!Intro_relations.contains(new Pair<>(directjar,npijar))) {
-////                            break;
-////                        }
-////                        Intro_relations.add(new Pair<>("二方库Jar包: "+directjar,npijar));
-//
-//                        System.out.println("二方库Jar包: " + directjar + " -> 孤立Jar包: " + npijar);
-//                        System.out.println("二方库Method: " + direct_class_method + " -> Method: " + npi_class_method);
-//                        return;
-//                    }
-//                }
-//            }
+                String own_class_methodstr = OWNJarsFunctions.get(ownjar);
+                String[] own_class_methods = own_class_methodstr.split(";");
+                for (String own_class_method : own_class_methods) {
+
+                    if (own_class_method.equals(edge.src().getDeclaringClass().getName() + "_" + edge.src().getName())) {
+
+                        System.out.println("一方库Jar包: " + ownjar + " -> 孤立Jar包: " + npijar);
+                        System.out.println("一方库Method: " + own_class_method + " -> Method: " + npi_class_method);
+                        flag = true;
+                    }
+                }
+
+            }
+            for (String directjar : DIRECTJarsFunctions.keySet()) {
+
+                String direct_class_methodstr = DIRECTJarsFunctions.get(directjar);
+                String[] direct_class_methods = direct_class_methodstr.split(";");
+                for (String direct_class_method : direct_class_methods) {
+                    if (direct_class_method.equals(edge.src().getDeclaringClass().getName() + "_" + edge.src().getName())) {
+
+
+                        System.out.println("二方库Jar包: " + directjar + " -> 孤立Jar包: " + npijar);
+                        System.out.println("二方库Method: " + direct_class_method + " -> Method: " + npi_class_method);
+                        flag = true;
+                    }
+                }
+            }
             for (String thirdjar : THIRDJarsFunctions.keySet()) {
 //                if(!Intro_relations.contains(new Pair<>(directjar,npijar))){
 //                    continue;
@@ -217,15 +207,32 @@ public class FunctionUtil {
 //                            break;
 //                        }
 //                        Intro_relations.add(new Pair<>("二方库Jar包: "+directjar,npijar));
-
-                        System.out.println("三方库Jar包: " + thirdjar + " -> 孤立Jar包: " + npijar);
-                        System.out.println("三方库Method: " + third_class_method + " -> Method: " + npi_class_method);
-                        return;
+                        pathJars.add(thirdjar);
+                        pathMethods.add(edge.src().getDeclaringClass().getName() + "_" + edge.src().getName());
+//                        System.out.println("三方库Jar包: " + thirdjar + " -> 孤立Jar包: " + npijar);
+//                        System.out.println("三方库Method: " + third_class_method + " -> Method: " + npi_class_method);
                     }
                 }
             }
+            if (flag) {
+                for (int i = 0; i<pathMethods.size(); i++
+                ) {
+                    System.out.println("中间三方包："+pathJars.get(i));
+
+                    System.out.println("中间三方包函数："+pathMethods.get(i));
+
+                }
+                return flag;
+            }
+
+            flag = findSourceMethod(edge.src(), newCg, npi_class_method, npijar, flag);
+
+
+
+
 
         }
+        return flag;
 
 
     }
@@ -246,9 +253,7 @@ public class FunctionUtil {
         SootExecutorUtil.setDefaultSootOptions(ClassPaths);
 
 
-//        默认先用CHA进行分析
         SootExecutorUtil.setSootEntryPoints(entrances);
-//        SootExecutorUtil.doCHAAanalysis();
 
         SootExecutorUtil.doFastSparkPointsToAnalysis(new HashMap<>(), CGType.VTA, null);
         CallGraph cg = Scene.v().getCallGraph();
@@ -266,61 +271,45 @@ public class FunctionUtil {
         for (Iterator<MethodOrMethodContext> iterator = Scene.v().getReachableMethods().listener(); iterator.hasNext(); ) {
             SootMethod method = (SootMethod) iterator.next();
 
-            NODE_LOGGER.info("{}: {} :{}", method.getBytecodeSignature(), method.getNumber(),method.getActiveBody());
-            for (Iterator<Edge> it = newCg.edgesOutOf(method); it.hasNext(); ) {
-                Edge edge = it.next();
-                EDGE_LOGGER.info("{} -> {}", edge.src().getNumber(), edge.tgt().getNumber());
-            }
-//            NODE_LOGGER.info("{}: {}", method.getBytecodeSignature(), method.getNumber());
-            boolean flag = false;
 
-
-            //如果该method的下层引用全是jdk的method
-            for (Iterator<Edge> it = newCg.edgesOutOf(method); it.hasNext(); ) {
-                Edge edge = it.next();
-                System.out.println();
-                if (edge.tgt().getName() == method.getName()) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag)
-                continue;
-            ;
+//            for (Iterator<Edge> it = newCg.edgesOutOf(method); it.hasNext(); ) {
+//                Edge edge = it.next();
+//                //如果该method的下层引用是jdk的同名method
+//                if (edge.tgt().getName() == method.getName() && edge.tgt().getDeclaringClass().getName() != method.getDeclaringClass().getName()) {
+//                    flag = true;
+//                    break;
+//                }
+//            }
+//            if (flag)
+//                continue;
+//            ;
 
             //过滤简单函数
-//            if(method.getName().equals("read")
-//            ||method.getName().equals("equals")
-//                    ||method.getName().equals("hash")
-//                    ||method.getName().equals("write")
-//                    ||method.getName().equals("call")
-//                    ||method.getName().equals("get")
-//                    ||method.getName().equals("add")
-//                    ||method.getName().equals("put")
-//                    ||method.getName().equals("next")
-//                    ||method.getName().equals("hasNext")
-//                    ||method.getName().equals("firstKey")
-//                    ||method.getName().equals("size")
-//                    ||method.getName().equals("hasnext")
-//                    ||method.getName().equals("toString")
-//                    ||method.getName().equals("<init>")
-//                    ||method.getName().equals("bind")
-//                    ||method.getName().equals("remove")
-//                    ||method.getName().equals("toArray")
-//                    ||method.getName().equals("indexOf")
-//                    ||method.getName().equals("iterator")
-//                    ||method.getName().equals("getValue")
-//                    ||method.getName().equals("getKey")
-//                    ||method.getName().equals("getKey")
-//                    ||method.getName().equals("getKey")
-//                    ||method.getName().equals("getKey")
-//                    ||method.getName().equals("getKey")
-//
-//
-//
-//
-//
-//
+//            if (method.getName().equals("read")
+//                    || method.getName().equals("equals")
+//                    || method.getName().equals("hash")
+//                    || method.getName().equals("write")
+//                    || method.getName().equals("call")
+//                    || method.getName().equals("get")
+//                    || method.getName().equals("add")
+//                    || method.getName().equals("put")
+//                    || method.getName().equals("next")
+//                    || method.getName().equals("hasNext")
+//                    || method.getName().equals("firstKey")
+//                    || method.getName().equals("size")
+//                    || method.getName().equals("hasnext")
+//                    || method.getName().equals("toString")
+//                    || method.getName().equals("bind")
+//                    || method.getName().equals("remove")
+//                    || method.getName().equals("toArray")
+//                    || method.getName().equals("indexOf")
+//                    || method.getName().equals("iterator")
+//                    || method.getName().equals("getValue")
+//                    || method.getName().equals("getKey")
+//                    || method.getName().equals("getKey")
+//                    || method.getName().equals("getKey")
+//                    || method.getName().equals("getKey")
+//                    || method.getName().equals("getKey")
 //
 //
 //            )
@@ -331,6 +320,7 @@ public class FunctionUtil {
 //            System.out.println("Methodtag:"+method.getDeclaringClass().getName()+"_"+method.getName());
 //            LOGGER.info("{}_{}", method.getDeclaringClass().getName(), method.getName());
 
+            boolean tag = false;
 
             for (Map.Entry<String, String> it : NPIJarsFunctions.entrySet()) {
                 String npijar = it.getKey();
@@ -344,14 +334,25 @@ public class FunctionUtil {
                         if (!findNPIJARs.contains(npijar)) {
                             findNPIJARs.add(npijar);
                             System.out.println("第" + findNPIJARs.size() + "个孤立jar包: " + npijar);
+                            System.out.println("找到的函数" + method.getDeclaringClass().getName() + "_" + method.getName());
 
                         }
 
-                        findSourceMethod(method, newCg, npi_class_method, npijar);
+                        tag = findSourceMethod(method, newCg, npi_class_method, npijar, tag);
+                        if (tag) {
+
+                                for (Iterator<Edge> itetrator = newCg.edgesOutOf(method); itetrator.hasNext(); ) {
+                                    Edge edge = itetrator.next();
+
+                                    System.out.println("source method:" + edge.tgt().getDeclaringClass().getName() + "_" + edge.tgt().getName());
+                                }
+                        }
+                        break;
 
 
                     }
                 }
+                if (tag) break;
             }
 
 
@@ -368,7 +369,6 @@ public class FunctionUtil {
 
 
     }
-
 
 
 }
